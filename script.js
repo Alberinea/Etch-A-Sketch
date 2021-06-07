0906552cae81e8a93bc23336fa1096bfdf5aa01a
Alberinea <kan9870@gmail.com>
2021-06-07 09:21:52 +0900

    Added clear and eraser functions

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('.container');
-const defSize = 800
+const clearButton = document.querySelector('#clear');
+const eraserButton = document.querySelector('#eraser');
+const pencilButton = document.querySelector('#pencil');
+let usingEraser = false;
+const defSize = 800;
 
 function createDiv() {
     for (let index = 0; index < defSize; index++) {
@@ -10,22 +14,65 @@ function createDiv() {
 }
 
 function black() {
-    this.style.backgroundColor = 'black'
+    this.style.backgroundColor = 'black';
+}
+
+function white() {
+    this.style.backgroundColor = 'white';
 }
 
 function draw(e) {
-    e.preventDefault(); 
-    const grids = document.querySelectorAll('.grid');
-    grids.forEach((grid) => grid.addEventListener('mousemove', black));
+    if (!usingEraser) {
+        e.preventDefault();
+        const grids = document.querySelectorAll('.grid');
+        grids.forEach((grid) => grid.addEventListener('mousemove', black));
+    }
 }
 
 function stopDraw(e) {
-    e.preventDefault(); 
-    const grids = document.querySelectorAll('.grid');  
-    grids.forEach((grid) => grid.removeEventListener('mousemove', black));
+    if (!usingEraser) {
+        e.preventDefault();
+        const grids = document.querySelectorAll('.grid');
+        grids.forEach((grid) => grid.removeEventListener('mousemove', black));
+    }
 }
 
+function reset() {
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach((grid) => (grid.style.backgroundColor = 'white'));
+}
+
+function eraserFlag() {
+    usingEraser = true;
+}
+
+function eraser(e) {
+    if (usingEraser) {
+        e.preventDefault();
+        const grids = document.querySelectorAll('.grid');
+        grids.forEach((grid) => grid.addEventListener('mousemove', white));
+    }
+}
+
+function stopEraser(e) {
+    console.log('a');
+    if (usingEraser) {
+        e.preventDefault();
+        const grids = document.querySelectorAll('.grid');
+        grids.forEach((grid) => grid.removeEventListener('mousemove', white));
+    }
+}
+
+
+function pencil() {
+    usingEraser = false;
+}
 
 createDiv();
 container.addEventListener('mousedown', draw);
 container.addEventListener('mouseup', stopDraw);
+container.addEventListener('mousedown', eraser);
+container.addEventListener('mouseup', stopEraser);
+pencilButton.addEventListener('click', pencil);
+clearButton.addEventListener('click', reset);
+eraserButton.addEventListener('click', eraserFlag);
